62d0ddbc6f43e4995873f3a91b48493072f0cb33
Ronny Recinos <36756626+Contra3@users.noreply.github.com>
2020-03-31 23:39:05 -0500

    Added AI and Levitate Spell

---
+++ Skoglendi_Academy_Final_Project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 100.0f;
+
+    // target
+    Transform target;
+    NavMeshAgent agent;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(target.position, transform.position);
+
+        if (dist <= lookRadius)
+        {
+            anim.SetInteger("battle", 1);
+            anim.SetInteger("moving", 2);
+            agent.SetDestination(target.position);
+        }
+    }
+
+    // draw the look radius within the editor to get an idea of it
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
+
---
+++ Skoglendi_Academy_Final_Project/Assets/Scripts/NaiveMeshColliderFix.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// NaiveMeshFix.cs - a script that naively creates collision bounds based on its mesh
+public class NaiveMeshColliderFix : MonoBehaviour
+{
+    public void Reset()
+    {
+        // create an empty mesh
+        Mesh mesh = null;
+
+        // grab the mesh of the mesh filter
+        var meshFilter = GetComponent<MeshFilter>();
+        // and its skinned mesh renderer
+        var skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+
+        // making sure things are not null
+        if (meshFilter == null)
+            throw new System.ArgumentException("Mesh Filter cannot be null");
+        // get the shared mesh of the mesh filter and apply it to the new mesh
+        mesh = meshFilter.sharedMesh;
+
+        if (skinnedMeshRenderer == null)
+            throw new System.ArgumentException("Skinned Mesh Renderer cannot be null");
+        // same thing as above but with the skinned mesh renderer instead
+        mesh = skinnedMeshRenderer.sharedMesh;
+
+        // if the mesh is null at this point, there's probably a problem
+        if (mesh != null)
+        {
+            // create a mesh collider that will wrap around the object
+            var collider = GetComponent<MeshCollider>();
+
+            // pretty sure this shouldn't be null
+            if (collider != null)
+                throw new System.ArgumentException("Collider is not null when should be null");
+
+            // make the collider = mesh bounds
+            collider = gameObject.AddComponent<MeshCollider>();
+            collider.sharedMesh = mesh;
+
+            // and recalculate the normals
+            mesh.RecalculateNormals();
+        }
+        else
+            throw new System.ArgumentException("Empty mesh");
+
+    }
+}
