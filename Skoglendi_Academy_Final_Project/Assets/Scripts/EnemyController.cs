daf2f5aa5c81a6a245aaf39c95e3209949fad318
Michael Hoang <MichaelJHoang@gmail.com>
2020-03-31 22:42:40 -0500

    added AI

---
+++ Skoglendi_Academy_Final_Project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 100.0f;
+
+    // target
+    Transform target;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(target.position, transform.position);
+
+        if (dist <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    // draw the look radius within the editor to get an idea of it
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
