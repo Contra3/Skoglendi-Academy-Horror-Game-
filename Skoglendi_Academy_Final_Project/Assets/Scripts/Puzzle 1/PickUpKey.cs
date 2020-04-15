a54021cf1cbc853161be5da1c6986b97e4925e3a
Ronny Recinos <36756626+Contra3@users.noreply.github.com>
2020-04-14 22:20:37 -0500

    fixed Pickup key issue

--- Skoglendi_Academy_Final_Project/Assets/Scripts/Puzzle 1/PickUpKey.cs
+++ Skoglendi_Academy_Final_Project/Assets/Scripts/Puzzle 1/PickUpKey.cs
@@ -19,7 +19,7 @@ public class PickUpKey : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && nearKey == true)
         {
             Destroy(gameObject);
-            UnlockDoor.haveKey = true;
+            PlayerController.haveKey = true;
             audio.Play(0);
         }
 
